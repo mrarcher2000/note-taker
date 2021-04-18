2b6546892fd5a4a545f79ad2d2a0a0ae8214aced
unknown <archernich@gmail.com>
2021-04-18 16:13:16 -0700

    delete notes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const notes = require('./db/db.json');
+const { response } = require('express');
 
 // set up server
 const PORT = process.env.PORT || 3001;
@@ -27,11 +28,34 @@ app.post('/api/notes', (req, res) => {
 
         fs.writeFileSync(
             path.join(__dirname, './db/db.json'),
-            JSON.stringify({notes}, null, 2)
+            JSON.stringify(notes, null, 2)
         );
 
         res.json(notes);
     }
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+
+    for (i = 0; i < notes.length; i++) {
+        currentNote = notes[i];
+        console.log(currentNote.id);
+        console.log(req.params.id);
+        if (currentNote.id == req.params.id) {
+            let pos = notes.indexOf(currentNote);
+            notes.splice(pos, 1);
+            console.log(notes);
+        }
+    };
+
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notes, null, 2)
+    );
+
+    res.send(`Note #${req.params.id} deleted`);
 });
 
 
